89bc9e969bb9035f31ffffd1ab52031d3191fba6
abportillo02 <abportillo@coh.org>
2025-10-23 16:42:55 -0700

    tree image

--- scripts/view_tree.py
+++ scripts/view_tree.py
@@ -1,10 +1,24 @@
 from ete3 import Tree, TreeStyle, TextFace
 
+# Load tree
 tree = Tree("/home/abportillo/github_repo/seq-align/mafft/trimmed_dmr_hervh_aligned.phy_phyml_tree.txt")
 
+# Load name mapping
+mapping = {}
+with open("/home/abportillo/github_repo/seq-align/mafft/name_mapping.tsv") as f:
+    for line in f:
+        short, full = line.strip().split("\t")
+        mapping[short] = full
+
+# Rename leaves using mapping
+for leaf in tree.iter_leaves():
+    if leaf.name in mapping:
+        leaf.name = mapping[leaf.name]
+
+# Tree style
 ts = TreeStyle()
 ts.show_leaf_name = True
-ts.title.add_face(TextFace("HERVH DMR Tree", fsize=20), column=0)
+ts.title.add_face(TextFace("HERVH Tree with Original Names", fsize=20), column=0)
 
-# Save tree as PNG image
-tree.render("/home/abportillo/github_repo/seq-align/mafft/hervh_dmr_tree.png", tree_style=ts, w=800, units="px")
+# Save tree image
+tree.render("/home/abportillo/github_repo/seq-align/mafft/hervh_tree_renamed.png", tree_style=ts, w=800, units="px")
