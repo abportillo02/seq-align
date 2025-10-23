1e9c0b02d638bb575f41e083197a9b4996b0cf41
abportillo02 <abportillo@coh.org>
2025-10-23 14:14:38 -0700

    mapping

---
+++ mapping.py
@@ -0,0 +1,17 @@
+from Bio import SeqIO
+import os
+
+input_fasta = "trimmed_dmr_hervh_aligned.fasta"
+output_dir = "/home/abportillo/github_repo/seq-align/mafft"
+os.makedirs(output_dir, exist_ok=True)
+
+output_fasta = os.path.join(output_dir, "renamed.fasta")
+mapping_file = os.path.join(output_dir, "name_mapping.tsv")
+
+with open(input_fasta) as infile, open(output_fasta, "w") as outfile, open(mapping_file, "w") as mapfile:
+    for i, record in enumerate(SeqIO.parse(infile, "fasta")):
+        new_id = f"HERVH{i:04d}"  # Unique PHYLIP-compatible ID
+        mapfile.write(f"{new_id}\t{record.id}\n")
+        record.id = new_id
+        record.description = ""
+        SeqIO.write(record, outfile, "fasta")
---
+++ scripts/run-mapping.sh
@@ -0,0 +1,19 @@
+!/bin/bash
+#SBATCH --job-name=mapping
+#SBATCH --output=/home/abportillo/github_repo/seq-align/mafft/mapping.out
+#SBATCH --error=/home/abportillo/github_repo/seq-align/mafft/mapping.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 16
+#SBATCH -N 1
+#SBATCH -p all
+#SBATCH --mem=50G
+#SBATCH --time=12:00:00
+
+source /home/abportillo/.bashrc
+conda activate /home/abportillo/.conda/envs/mamba_abner_BC
+
+
+
+# Run Python script
+python mapping.py
