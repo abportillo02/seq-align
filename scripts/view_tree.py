f611872ca63b1c83e44039184896953fa58ff2fc
abportillo02 <abportillo@coh.org>
2025-10-23 16:04:45 -0700

    tree

--- scripts/view_tree.py
+++ scripts/view_tree.py
@@ -1,12 +1,10 @@
-from ete3 import Tree, TreeStyle
+from ete3 import Tree, TreeStyle, TextFace
 
-# Load the tree from your PhyML output
 tree = Tree("/home/abportillo/github_repo/seq-align/mafft/trimmed_dmr_hervh_aligned.phy_phyml_tree.txt")
 
-# Customize tree style
 ts = TreeStyle()
 ts.show_leaf_name = True
 ts.title.add_face(TextFace("HERVH DMR Tree", fsize=20), column=0)
 
-# Show the tree
-tree.show(tree_style=ts)
+# Save tree as PNG image
+tree.render("hervh_dmr_tree.png", tree_style=ts, w=800, units="px")
