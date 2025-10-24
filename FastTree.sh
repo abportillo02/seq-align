b5e4d3ffad0c3dd63b70cfbb9b47dcd9be43a4ed
abportillo02 <abportillo@coh.org>
2025-10-24 12:22:10 -0700

    Fastree

---
+++ FastTree.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=FastTree
+#SBATCH --output=/home/abportillo/github_repo/seq-align/mafft/FastTree.out
+#SBATCH --error=/home/abportillo/github_repo/seq-align/mafft/FastTree.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 16
+#SBATCH -N 1
+#SBATCH -p all
+#SBATCH --mem=50G
+#SBATCH --time=12:00:00
+
+source /home/abportillo/.bashrc
+conda activate /home/abportillo/.conda/envs/mamba_abner_BC
+
+FastTree -nt /home/abportillo/github_repo/seq-align/mafft/trimmed_dmr_hervh_subset_aligned_int.phy > /home/abportillo/github_repo/seq-align/mafft/tree_subset.nwk
