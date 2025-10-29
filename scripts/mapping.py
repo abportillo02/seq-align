5fabc482ea4d2b5bd514fd618189d3bde06e7b59
abportillo02 <abportillo@coh.org>
2025-10-29 11:43:58 -0700

    maping update

--- scripts/mapping.py
+++ scripts/mapping.py
@@ -2,12 +2,12 @@ from Bio import SeqIO
 import os
 
 # input_fasta = "/home/abportillo/github_repo/seq-align/mafft/dmr_hervh_aligned.fasta" orignal analysis
-input_fasta = "/home/abportillo/github_repo/seq-align/mafft/dmr_hervh_subset_aligned.fasta"
+input_fasta = "/home/abportillo/github_repo/seq-align/mafft/real_dmr_hervh_subset.fasta"
 output_dir = "/home/abportillo/github_repo/seq-align/mafft"
 os.makedirs(output_dir, exist_ok=True)
 
-output_fasta = os.path.join(output_dir, "dmr_hervh_subset_aligned_int.fasta")
-mapping_file = os.path.join(output_dir, "name_mapping_subset.tsv")
+output_fasta = os.path.join(output_dir, "real_dmr_hervh_subset_aligned_int.fasta")
+mapping_file = os.path.join(output_dir, "real_name_mapping_subset.tsv")
 
 with open(input_fasta) as infile, open(output_fasta, "w") as outfile, open(mapping_file, "w") as mapfile:
     for i, record in enumerate(SeqIO.parse(infile, "fasta")):
