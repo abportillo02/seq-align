18eca6780655b4d36f767de195c66fbfe9ba8dbe
abportillo02 <abportillo@coh.org>
2025-10-29 11:57:03 -0700

    more .mapping

---
+++ scripts/run-more-mapping.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=more-mapping
+#SBATCH --output=/home/abportillo/github_repo/seq-align/mafft/more-mapping.out
+#SBATCH --error=/home/abportillo/github_repo/seq-align/mafft/more-mapping.err
+#SBATCH --mail-type=END,FAIL
+#SBATCH --mail-user=abportillo@coh.org
+#SBATCH -n 16
+#SBATCH -N 1
+#SBATCH -p all
+#SBATCH --mem=50G
+#SBATCH --time=12:00:00
+
+source /home/abportillo/.bashrc
+conda activate /home/abportillo/.conda/envs/mamba_abner_BC
+
+# Run Python script
+python /home/abportillo/github_repo/seq-align/scripts/more.mapping.py
